f7bd98043445540a55096f0d1fe25b9b02fddf62
Neil Lindquist <neillindquist5@gmail.com>
2020-03-21 16:06:30 -0500

    Add control of n and tests to the command line

--- main.cpp
+++ main.cpp
@@ -25,10 +25,19 @@ void run_tests(size_t n, size_t tests) {
 }
 
 int main(int argc, char* argv[]) {
-  Kokkos::initialize();
+	Kokkos::initialize();
 
-  run_tests(1000*1000, 10);
+	if (argc < 3) {
+		printf("Arguments: N tests\n");
+		printf("  N:   Number of vector entries\n");
+		printf("  tests: number of times each kernels should be run\n");
+	}
 
-  Kokkos::finalize();
+	const size_t n = atoi(argv[1]);
+	const size_t tests = atoi(argv[2]);
+
+	run_tests(n, tests);
+
+	Kokkos::finalize();
 }
 
